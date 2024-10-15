a54e47d59ebc873c3dbda181d6e34c4b4ed82b3e
alex <alex88.b@icloud.com>
2024-10-15 19:33:54 +0300

    hw2 initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
 function App() {
+
+  let[usersList, setUsersList] = useState<any>([]);
+
+  useEffect(() => {
+      fetch('https://dummyjson.com/users')
+        .then(response => response.json())
+        .then(data => {
+          setUsersList(data.users);
+          console.log(usersList);
+        });
+    }, []);
+
+
   return (
     <>
 
