08f27d027075ba2cf1bd83621893e3d794e288e7
alex <alex88.b@icloud.com>
2024-10-28 18:02:30 +0200

    hw5 dummy json post a post error

--- src/services/putPost.ts
+++ src/services/putPost.ts
@@ -6,12 +6,12 @@ export const putPost = async (dataFromForm:IFormModel) => {
     const {title, body} = dataFromForm;
 
     return await axiosInstanse.post('posts/add', {
-        headers: {'Content-type': 'application/json'},
+            headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
                 title: title,
                 userId: 5,
                 body: body
             })
         })
-        .then(response => {return response})
+        .then(response => {return response.data})
 }
